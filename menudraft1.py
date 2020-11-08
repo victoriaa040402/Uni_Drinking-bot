5d5777c494532cc8acf312bf4aae989680771a0c
victoriaa040402 <72193031+victoriaa040402@users.noreply.github.com>
2020-11-08 16:31:20 +0000

    Create menudraft1.py

---
+++ menudraft1.py
@@ -0,0 +1,11 @@
+print("Welcome! I am the bot for drinking games.")
+print("Due to COVID-19 restrictions you can only have a maximum of 10 players")
+list=[]
+n=int(input("Enter number of players:"))
+for i in range(0,n):
+    name=str(input("Enter you name:"))
+    list.append(name)
+print("Welcome: " + ', '.join(list))
+print("1.Jacks Drinking Game")
+print("2.Anna's Drinking Game")
+game=input("What game would you like to play: ")
